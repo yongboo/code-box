d9458b6c7ba353bfe88d50c2a734c679bbf6cb26
yongboo <shanyongbo2008@gmail.com>
2018-06-13 17:19:56 +0800

    初始化项目

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+var person = {
+  name: 'shanyongbo',
+  age: 27
+}
+for(var item in person){
+  console.log(item + ':' + person[item])
+}
---
+++ src/md/HTML.md
@@ -0,0 +1,5 @@
+## html轮子说明
+
+ ### 1. [001.html](../html/001.html) 设置html全局 font-size方法
+
+
