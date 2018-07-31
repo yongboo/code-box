b8195c9debc6ac568d23126e700e8645585606e8
yongboo <shanyongbo2008@gmail.com>
2018-07-31 22:04:31 +0800

    调整目录结构

---
+++ js_wheels/README.md
@@ -0,0 +1,3 @@
+## html轮子说明
+
+ ### 1. [001.html](./src/html/001.html) 设置html全局 font-size方法
---
+++ js_wheels/src/js/001.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill'
+import '../css/001.scss'
+class Person {
+  constructor(name, age) {
+    this.name = name,
+    this.age = age
+  }
+  
+  sayHello() {
+    console.log(this.name + 'say hello')
+  }
+}
+let shanyongbo = new Person('shanyongbo', 30)
+shanyongbo.sayHello()
+
+console.log(typeof Person)
+console.log(Person.prototype.constructor === Person)
+if(module.hot) {
+  console.log('module is updateing')
+}
