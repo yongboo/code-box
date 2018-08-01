2a881b3bf62536b1e14fb2c224b6a0779aef3dea
yongboo <shanyongbo2008@gmail.com>
2018-08-01 22:35:07 +0800

    update

---
+++ vue_wheels/vue_source_code/src/js/001.js
@@ -0,0 +1,40 @@
+function cb (newVal) {
+  console.log('试图更新了')
+}
+
+// 这个方法通过 Object.defineProperty 来实现对对象的「响应式」化
+function defineReactive (obj, key, val) {
+  Object.defineProperty (obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      return val
+    },
+    set: function (newVal) {
+      if (newVal === val) return
+      cb(newVal)
+    }
+  })
+}
+
+function observe (obj) {
+  if (!obj || (typeof obj !== 'object')) return
+  Object.keys(obj).forEach((key) => {
+    defineReactive(obj, key, obj[key]);
+  });
+}
+
+class Vue {
+  constructor (options) {
+    this._data = options.data
+    observe(this._data)
+  }
+}
+
+let _vue = new Vue({
+  data: {
+    test: 'hi xiaoming'
+  }
+})
+
+_vue._data.test = 'hello world!'
