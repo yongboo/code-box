de40c7431e7a747e94988ee371b6d85a03b43f0d
yongboo <shanyongbo2008@gmail.com>
2018-10-12 18:38:45 +0800

    add react demo

---
+++ react/my-app/src/components/todo/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>hello this is a todo list</div>
+    )
+  }
+}
+
+export default Todo
